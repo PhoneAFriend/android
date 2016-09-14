7f05dbd4d65c765737af4159cf8e813ed4c196aa
Raul <barahonaraul@live.com>
2016-09-14 15:28:03 -0400

    Renamed files and added view componenets

--- app/src/main/java/seniordesign/phoneafriend/SignUp.java
+++ app/src/main/java/seniordesign/phoneafriend/SignUp.java
@@ -3,11 +3,11 @@ package seniordesign.phoneafriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
     }
 }
