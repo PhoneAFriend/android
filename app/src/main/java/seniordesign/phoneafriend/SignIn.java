f5b4ed15d7dd3bc5f2e111cbb216481ff30ffb2b
Peter Camejo <thealex123@gmail.com>
2016-09-22 01:17:34 -0400

    Added switching from signin to sign up. Fixed sign in text typo

--- app/src/main/java/seniordesign/phoneafriend/SignIn.java
+++ app/src/main/java/seniordesign/phoneafriend/SignIn.java
@@ -1,7 +1,9 @@
 package seniordesign.phoneafriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    protected void gotoSignUp(View view){
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
