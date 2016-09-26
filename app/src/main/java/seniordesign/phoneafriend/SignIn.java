9691619f7bffe0e03e3474ad1a49139068c90461
Peter Camejo <thealex123@gmail.com>
2016-09-25 20:09:30 -0400

    Project runs again

--- app/src/main/java/seniordesign/phoneafriend/SignIn.java
+++ app/src/main/java/seniordesign/phoneafriend/SignIn.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.firebase.client.Firebase;
+
 public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        Firebase.setAndroidContext(this);
     }
 
     protected void gotoSignUp(View view){
