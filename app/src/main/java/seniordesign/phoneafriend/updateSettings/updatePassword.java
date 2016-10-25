6f370deaa5169c9dcb600345e01f220b1b33b017
Raul <barahonaraul@live.com>
2016-10-24 22:49:01 -0400

    Completed Layout for updating password

---
+++ app/src/main/java/seniordesign/phoneafriend/updateSettings/updatePassword.java
@@ -0,0 +1,15 @@
+package seniordesign.phoneafriend.updateSettings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import seniordesign.phoneafriend.R;
+
+public class updatePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_password_layout);
+    }
+}
