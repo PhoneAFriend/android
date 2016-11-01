7bfdd36ed0c00f86b92835520a6a2289cfd01ab6
Raul <barahonaraul@live.com>
2016-10-31 21:41:47 -0400

    Got Search Making Toast messages of whatever is inputted. Will now proceed to looking at database.

--- app/src/main/java/seniordesign/phoneafriend/userSearch/searchUsers.java
+++ app/src/main/java/seniordesign/phoneafriend/userSearch/searchUsers.java
@@ -1,15 +1,51 @@
 package seniordesign.phoneafriend.userSearch;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 import seniordesign.phoneafriend.R;
 
 public class searchUsers extends AppCompatActivity {
 
+    private SearchView searcher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_users);
+
+        //Get the SearchView Widget
+        searcher = (SearchView) findViewById(R.id.search_user);
+
+
+        //Create a listener for when the query text is changed
+        SearchView.OnQueryTextListener searchListener = new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // What to do if there is any text change
+                return false;
+            }
+
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // What to do when user submits the query string
+                //Output the new list with the query results
+                Toast.makeText(searchUsers.this, searcher.getQuery(), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        };
+        //add the listener to our widget
+        searcher.setOnQueryTextListener(searchListener);
+
+    }
+
+    private void searchUserList(){
+        Toast.makeText(searchUsers.this,searcher.getQuery(),Toast.LENGTH_LONG).show();
     }
 }
